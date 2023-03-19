ab944a2009ce31b3b533498211be9b28a6ce53f3
Dhritideepa Gorai <dhritideepagorai@gmail.com>
2023-03-19 10:52:31 +0530

    chatgpt codes

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const yesBtn = document.querySelector('.yesbtn')
 const noBtn = document.querySelector('.nobtn')
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', function() {
+  menu.classList.toggle('show');
+});
 
 yesBtn.addEventListener('click',()=>{
   alert("YESSSS")
