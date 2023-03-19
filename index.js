d985cd2bd08d0040123aa080635d01aa023acc6b
Dhritideepa Gorai <dhritideepagorai@gmail.com>
2023-03-19 10:38:27 +0530

    dhriti's changes

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('.yesbtn')
+const noBtn = document.querySelector('.nobtn')
+
+yesBtn.addEventListener('click',()=>{
+  alert("YESSSS")
+})
+
+noBtn.addEventListener('click',()=>{
+  alert("NO")
+})
+
